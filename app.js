daa9fb32bbc8843807dde9c788f409bfbb47190f
shela-2226250087 <vicinskyowentanner_2327240031@mhs.mdp.ac.id>
2025-10-25 18:27:20 +0700

    Phase2

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "OK"
+    })
+})
+
 app.listen(3000, () => {
     console.log('Aplikasi berjalan di http://localhost:3000')
 })
---
+++ models/movieModels.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema (
+    {
+        judul: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        tahunRilis: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        sutradara: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const movieModel = mongoose.model("movie", movieSchema)
+
+export default movieModel
