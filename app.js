b5738cce178cc2c4c0a18bc0d8c3c184ab3534a3
2327240031MDP <vicinskyowentanner_2327240031@mhs.mdp.ac.id>
2025-11-15 18:13:02 +0700

    install cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import api from "./routes/api.js"
+import cors from "cors"
 import { database } from "./config/database.js"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/api", api)
